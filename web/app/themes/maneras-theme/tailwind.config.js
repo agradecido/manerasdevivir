d9772ef015444e8b1a2b9dae43998d2e85120adb
Javier Sierra <javier.sierra@oc.vasscompany.com>
2025-06-08 14:23:17 +0200

    styles: add breadcrumbs to layout & limit reading content width

--- web/app/themes/maneras-theme/tailwind.config.js
+++ web/app/themes/maneras-theme/tailwind.config.js
@@ -28,7 +28,15 @@ module.exports = {
       container: {
         center: true,
         padding: '1rem',
-      },     
+      },
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      rc: '800px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
     },
   },
   plugins: [],
